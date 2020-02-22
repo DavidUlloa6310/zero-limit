6ed32fa8d182bbcd6b9e2c2db5b9d79be5969702
David Ulloa <41076449+DavidUlloa6310@users.noreply.github.com>
2020-02-22 16:29:44 -0500

    Add files via upload

--- zero_limit/routes.py
+++ zero_limit/routes.py
@@ -1,8 +1,6 @@
-from sqlalchemy.exc import IntegrityError
-from zero_limit.models import customer, location
-from zero_limit.forms import reservation_form
-from flask import redirect, render_template, flash
-from zero_limit import app, db
+from zero_limit.models import location
+from flask import render_template
+from zero_limit import app
 
 @app.route('/home')
 @app.route('/')
@@ -12,17 +10,3 @@ def home():
 @app.route('/store')
 def store():
     return render_template('store.html', location = location)
-
-@app.route('/reservation/<location_name>', methods=['GET','POST'])
-def reservation(location_name):
-    form = reservation_form()
-    if form.validate_on_submit():
-        new_customer = customer(parent_name = form.parent_name.data, child_name = form.child_name.data, email = form.email.data, location=location.query.filter(location.name=="The Citadel").first())
-        db.session.add(new_customer)
-        try:
-            db.session.commit()
-            flash(f"Thank you. Your reservation for {form.child_name.data} has been received and we've sent you a confirmation email at {form.email.data}. ")
-        except IntegrityError:
-            db.session.rollback()
-            flash(f"{form.child_name.data} already has a reservation.", 'Error')
-    return render_template('reservation.html', location_name=location_name, form=form)
